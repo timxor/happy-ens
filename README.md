0469ca275a76b6c487740fc7d14cceac0e052938
Tim Siwula <tim.siwula@consensys.net>
2017-10-14 10:14:13 -0400

    update docs

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # happy_ens
-Resolve *.eth domains in your browser with our dns server
 
-# Step 1) -- Add our custom dns server ip address (138.197.150.21) to your computers network settings
-the ip address 138.197.150.21 as your DNS server
-A java search engine that supports crawling, indexing, partial search and exact search.
-![How to update your mac dns server ip address](h-ens.gif)
+## Resolve *.eth domains in your browser with our dns server
+
+## Step 1) -- Update your computers DNS server
+
+*Set your computers DNS server to address: 138.197.150.21*
+
+## Step 2) -- Register your mydomain.eth and its resource
+
+*Go to [happyethereum.com/register](www.happyethereum.com/register) and
+associate your .eth domain with a ipfs resource to your decentralized website*
+
+![mac gif tutorial](h-ens.gif)
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+let ui = require('./ui/index.js')
 let dns = require('native-dns');
 let server = dns.createServer();
+let async = require('async');
 
 server.on('listening', () => console.log('server listening on', server.address()));
 server.on('close', () => console.log('server closed', server.address()));
@@ -9,3 +11,62 @@ server.on('error', (err, buff, req, res) => console.error(err.stack));
 server.on('socketError', (err, socket) => console.error(err));
 
 server.serve(53);
+
+// use googles dns servers as default, unless *.eth found in dns request
+let authority = {
+	address: '8.8.8.8',
+	port: 53,
+	type: 'udp'
+};
+
+function proxy(question, response, cb) {
+	var request = dns.Request({
+		question: question, // forwarding the question
+		server: authority, // this is the DNS server we are asking
+		timeout: 1000
+	});
+
+
+	request.on('timeout', function() {
+		console.log('Timeout in making request no forwarding', question.name);
+	});
+
+	// when we get answers, append them to the response
+	request.on('message', (err, msg) => {
+		msg.answer.forEach(a => {
+			response.answer.push(a);
+		});
+	});
+
+	request.on('end', cb);
+	request.send();
+}
+
+function handleRequest(request, response) {
+	var question = request.question[0];
+	console.log('questions', request.question);
+	let f = [];
+
+	request.question.forEach(question => {
+		let entry = ui.entries.filter(r => new RegExp(r.domain, 'i').exec(question.name));
+
+		// a local resolved host
+		if (question.name.includes(".eth")) {
+			let record = {};
+			record.name = question.name;
+			record.ttl = record.ttl || 1800;
+			record.address = "147.135.130.181";
+			response.answer.push(dns["A"](record));
+		} else {
+			// forwarding host
+			console.log("forwarding host");
+			f.push(cb => proxy(question, response, cb));
+		}
+	});
+
+	async.parallel(f, function() {
+		response.send();
+	});
+}
+
+server.on('request', handleRequest);
